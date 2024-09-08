2d41e889e8e11f842e66a5b9eea0ffcf5aab1b09
Kamelia <143409916+KameliaSGithub@users.noreply.github.com>
2024-09-08 20:39:09 +0100

    SortNumbers.java

---
+++ SortNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SortNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        int[] numbers = {num1, num2, num3};
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - 1 - i; j++) { //-i cause the sec numj going down to i
+                if (numbers[j] < numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+        for (int number : numbers) {
+            System.out.println(number);
+        } 
+        scanner.close();
+    }
+}
